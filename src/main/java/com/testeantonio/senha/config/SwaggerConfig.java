3287b2ce4c84f1115d3deff25e3015354f00df54
Antonio Costa <antonio.gcn@gmail.com>
2021-05-18 20:48:25 -0300

    swagger

--- src/main/java/com/testeantonio/senha/config/SwaggerConfig.java
+++ src/main/java/com/testeantonio/senha/config/SwaggerConfig.java
@@ -16,6 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
           .select()
+          .apis(RequestHandlerSelectors.basePackage("com.testeantonio.senha.controller"))
           .apis(RequestHandlerSelectors.any())
           .paths(PathSelectors.any())
           .build();
